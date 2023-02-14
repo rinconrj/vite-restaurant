3eb0cbc99bac835ab99ebf42213dee800dc27339
Ricardo Villalobos <rinconrj@gmail.com>
2023-02-13 22:55:01 -0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <div>marca</div>
+      <div>menu</div>
+      <div>perfil</div>
+    </div>
+  )
+}
+
+export default Header
